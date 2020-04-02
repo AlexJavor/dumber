15f3ef97281e8dba1946b85799e92824857b7419
AlexJavor <aleksanjavor@gmail.com>
2020-04-02 12:36:53 +0200

    Feature 13: Update battery level every 500ms - Completed

--- software/raspberry/superviseur-robot/tasks.cpp
+++ software/raspberry/superviseur-robot/tasks.cpp
@@ -26,6 +26,7 @@
 #define PRIORITY_TRECEIVEFROMMON 25
 #define PRIORITY_TSTARTROBOT 20
 #define PRIORITY_TCAMERA 21
+#define PRIORITY_TBATTERY 26 //Possible value change
 
 /*
  * Some remarks:
@@ -123,6 +124,10 @@ void Tasks::Init() {
         cerr << "Error task create: " << strerror(-err) << endl << flush;
         exit(EXIT_FAILURE);
     }
+    if (err = rt_task_create(&th_battery, "th_battery", 0, PRIORITY_TBATTERY, 0)) {
+        cerr << "Error task create: " << strerror(-err) << endl << flush;
+        exit(EXIT_FAILURE);
+    }
     cout << "Tasks created successfully" << endl << flush;
 
     /**************************************************************************************/
@@ -164,6 +169,10 @@ void Tasks::Run() {
         exit(EXIT_FAILURE);
     }
     if (err = rt_task_start(&th_move, (void(*)(void*)) & Tasks::MoveTask, this)) {
+        cerr << "Error task start: " << strerror(-err) << endl << flush;
+        exit(EXIT_FAILURE);
+    }
+    if (err = rt_task_start(&th_battery, (void(*)(void*)) & Tasks::BatteryTask, this)) {
         cerr << "Error task start: " << strerror(-err) << endl << flush;
         exit(EXIT_FAILURE);
     }
@@ -378,6 +387,46 @@ void Tasks::MoveTask(void *arg) {
             rt_mutex_acquire(&mutex_robot, TM_INFINITE);
             robot.Write(new Message((MessageID)cpMove));
             rt_mutex_release(&mutex_robot);
+        }
+        cout << endl << flush;
+    }
+}
+/**
+ * @brief Thread handling the battery's level output 
+ */
+void Tasks::BatteryTask(void *arg){
+    int rs;
+    Message* batteryLevel;
+    MessageID batteryLevelID;
+    
+    cout << "Start " << __PRETTY_FUNCTION__ << endl << flush;
+    // Synchronization barrier (waiting that all tasks are starting)
+    rt_sem_p(&sem_barrier, TM_INFINITE);
+    
+    /**************************************************************************************/
+    /* The task starts here                                                               */
+    /**************************************************************************************/
+    
+    rt_task_set_periodic(NULL, TM_NOW, 500000000);
+    
+    while(1){
+        rt_task_wait_period(NULL);
+        cout << "Periodic battery level check";
+        rt_mutex_acquire(&mutex_robotStarted, TM_INFINITE);
+        rs = robotStarted;
+        rt_mutex_release(&mutex_robotStarted);
+        // Check if the robot has started
+        if (rs == 1){
+            rt_mutex_acquire(&mutex_robot, TM_INFINITE);
+            batteryLevel = robot.Write(new Message(MESSAGE_ROBOT_BATTERY_GET));
+            rt_mutex_release(&mutex_robot);
+            
+            // Get the battery level MessageID
+            batteryLevelID = batteryLevel->GetID();
+            // Write the battery level in the q_messageToMon queue
+            if(batteryLevelID == MESSAGE_ROBOT_BATTERY_LEVEL){
+                WriteInQueue(&q_messageToMon,batteryLevel);
+            }
         }
         cout << endl << flush;
     }
